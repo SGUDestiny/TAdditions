42a940dd627dc85e0ecd7b6e2bf8e79ed27994e1
mistersecret312 <sergiigamer7@gmail.com>
2023-11-18 14:44:10 +0200

    Mars Fixed

---
+++ src/main/java/net/tadditions/mixin/SpaceDimCapMixin.java
@@ -0,0 +1,25 @@
+package net.tadditions.mixin;
+
+
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.World;
+import net.tadditions.mod.world.MDimensions;
+import net.tardis.mod.cap.SpaceDimensionCapability;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpaceDimensionCapability.class)
+public class SpaceDimCapMixin {
+
+    @Shadow private RegistryKey<World> world;
+
+    @Inject(method = "hasAir", at = @At("HEAD"), cancellable = true)
+    private void onHasAir(CallbackInfoReturnable<Boolean> callbackInfo) {
+        if (this.world == MDimensions.MARS) {
+            callbackInfo.setReturnValue(false);
+        }
+    }
+}
