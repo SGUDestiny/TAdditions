64ba6b2c558d0938c6605dca45c7094ef534099c
mistersecret312 <sergiigamer7@gmail.com>
2023-12-26 13:02:47 +0200

    Introvert and Extrovert Traits, the most complex yet

---
+++ src/main/java/net/tadditions/mod/traits/ExtrovertTrait.java
@@ -0,0 +1,43 @@
+package net.tadditions.mod.traits;
+
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Direction;
+import net.minecraft.util.concurrent.TickDelayedTask;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.tardis.mod.blocks.exteriors.ExteriorBlock;
+import net.tardis.mod.tileentities.ConsoleTile;
+import net.tardis.mod.tileentities.console.misc.EmotionHandler.EnumHappyState;
+import net.tardis.mod.tileentities.exteriors.ExteriorTile;
+import net.tardis.mod.traits.TardisTrait;
+import net.tardis.mod.traits.TardisTraitType;
+
+public class ExtrovertTrait extends TardisTrait {
+
+	public ExtrovertTrait(TardisTraitType type) {
+		super(type);
+	}
+
+	@Override
+	public void tick(ConsoleTile tile) {
+		if (!tile.getWorld().isRemote()) {
+			if (!tile.isInFlight() && !tile.isLanding()) {
+				ExteriorTile ext = tile.getExteriorType().getExteriorTile(tile);
+				World world = ext.getWorld();
+				AxisAlignedBB area = new AxisAlignedBB(ext.getPos()).grow(16);
+				for (BlockPos blockPos : BlockPos.getAllInBoxMutable((int) area.minX, (int) area.minY, (int) area.minZ, (int) area.maxX, (int) area.maxY, (int) area.maxZ)) {
+					if (world.getBlockState(blockPos).getBlock() instanceof ExteriorBlock) {
+						if(tile.getWorld().getGameTime() % 200*this.getModifier() == 0){
+							tile.getEmotionHandler().addMood(1);
+							this.warnPlayer(tile, new TranslationTextComponent("tadditions.likes_company"));
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ src/main/java/net/tadditions/mod/traits/IntrovertTrait.java
@@ -0,0 +1,51 @@
+package net.tadditions.mod.traits;
+
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.tardis.mod.blocks.exteriors.ExteriorBlock;
+import net.tardis.mod.helper.TardisHelper;
+import net.tardis.mod.tileentities.ConsoleTile;
+import net.tardis.mod.tileentities.exteriors.ExteriorTile;
+import net.tardis.mod.traits.TardisTrait;
+import net.tardis.mod.traits.TardisTraitType;
+
+import java.util.Arrays;
+
+public class IntrovertTrait extends TardisTrait {
+
+	public IntrovertTrait(TardisTraitType type) {
+		super(type);
+	}
+
+	@Override
+	public void tick(ConsoleTile tile) {
+		if (!tile.getWorld().isRemote()) {
+			if (!tile.isInFlight() && !tile.isLanding()) {
+				ExteriorTile ext = tile.getExteriorType().getExteriorTile(tile);
+				World world = ext.getWorld();
+				AxisAlignedBB area = new AxisAlignedBB(ext.getPos()).grow(16);
+				for (BlockPos blockPos : BlockPos.getAllInBoxMutable((int) area.minX, (int) area.minY, (int) area.minZ, (int) area.maxX, (int) area.maxY, (int) area.maxZ)) {
+					if (world.getBlockState(blockPos).getBlock() instanceof ExteriorBlock) {
+						if (tile.getWorld().getGameTime() % 200 * this.getModifier() == 0) {
+							ExteriorTile extile = (ExteriorTile) world.getTileEntity(blockPos);
+							ConsoleTile intile = TardisHelper.getConsole(world.getServer(), extile.getInteriorDimensionKey()).orElse(null);
+							if (intile != null) {
+								if (Arrays.stream(intile.getEmotionHandler().getTraits()).anyMatch(trait -> trait.equals(MTraits.INTROVERT.get().create()))) {
+									intile.getEmotionHandler().addMood(1);
+									tile.getEmotionHandler().addMood(1);
+								}
+								else if (Arrays.stream(intile.getEmotionHandler().getTraits()).anyMatch(trait -> trait.equals(MTraits.EXTROVERT.get().create()))) {
+									tile.getEmotionHandler().addMood(-2);
+								}
+
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
