46cb573cec488ff18df803e42f14a06b3963e19a
mistersecret312 <sergiigamer7@gmail.com>
2024-04-10 22:40:58 +0300

    What a dated voice!

---
+++ src/main/java/net/tadditions/mixin/EmotionHandlerMixin.java
@@ -0,0 +1,13 @@
+package net.tadditions.mixin;
+
+import net.tardis.mod.tileentities.console.misc.EmotionHandler;
+import net.tardis.mod.traits.TardisTrait;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EmotionHandler.class)
+public interface EmotionHandlerMixin {
+
+    @Accessor("traits")
+    void setTraits(TardisTrait[] traits);
+}
--- src/main/java/net/tadditions/mixin/ExteriorMixin.java
+++ src/main/java/net/tadditions/mixin/ExteriorMixin.java
@@ -8,27 +8,35 @@ import net.tadditions.mod.helper.IExteriorHelp;
 import net.tardis.mod.tileentities.ConsoleTile;
 import net.tardis.mod.tileentities.exteriors.ExteriorTile;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ExteriorTile.class)
-public class ExteriorMixin implements IExteriorHelp {
+public abstract class ExteriorMixin implements IExteriorHelp {
+
+    @Shadow public abstract void updateClient();
 
     private CloakState cloakState = CloakState.UNCLOAKED;
     private int cloakAnimTime = 0;
 
     @Inject(at = @At(value = "HEAD"), method = "read(Lnet/minecraft/block/BlockState;Lnet/minecraft/nbt/CompoundNBT;)V")
     public void read(BlockState state, CompoundNBT compound, CallbackInfo ci) {
-        this.cloakState = CloakState.values()[compound.getInt("cloakState")];
+        this.cloakState = CloakState.valueOf(compound.getString("cloak"));
         this.cloakAnimTime = compound.getInt("cloakAnimTime");
     }
 
     @Inject(at = @At(value = "HEAD"), method = "write(Lnet/minecraft/nbt/CompoundNBT;)Lnet/minecraft/nbt/CompoundNBT;")
     public void write(CompoundNBT compound, CallbackInfoReturnable<CompoundNBT> cir) {
-        compound.putInt("cloakState", this.cloakState.ordinal());
+        compound.putString("cloak", this.cloakState.name());
         compound.putInt("cloakAnimTime", this.cloakAnimTime);
+    }
+
+    @Inject(at = @At(value = "HEAD"), method = "getUpdateTag()Lnet/minecraft/nbt/CompoundNBT;")
+    public void updateTag(CallbackInfoReturnable<CompoundNBT> cir){
+        cir.getReturnValue().putInt("cloakState", this.cloakState.ordinal());
     }
 
     @Inject(at = @At(value = "HEAD"), method = "tick()V")
@@ -57,6 +65,8 @@ public class ExteriorMixin implements IExteriorHelp {
     @Override
     public void setCloakState(CloakState cloakedState) {
         this.cloakState = cloakedState;
+        ((ExteriorTile) (Object) this).markDirty();
+        this.updateClient();
         this.cloakAnimTime = 0;
     }
 
