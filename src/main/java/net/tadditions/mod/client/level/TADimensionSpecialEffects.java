433661b7542ded9b15617c5f61a8a4d9eca44680
mistersecret312 <sergiigamer7@gmail.com>
2024-10-14 21:16:55 +0300

    Mars Sky + Stellar View compatability.

--- src/main/java/net/tadditions/mod/client/level/TADimensionSpecialEffects.java
+++ src/main/java/net/tadditions/mod/client/level/TADimensionSpecialEffects.java
@@ -1,15 +1,24 @@
 package net.tadditions.mod.client.level;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Axis;
 import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.DimensionSpecialEffects;
-import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import net.minecraftforge.fml.ModList;
+import net.povstalec.sgjourney.client.render.level.SGJourneySkyRenderer;
+import net.povstalec.stellarview.client.resourcepack.ViewCenters;
 import net.tadditions.mod.TemporalAdditionsMod;
 import org.joml.Matrix4f;
+
+import javax.annotation.Nullable;
 
 public class TADimensionSpecialEffects extends DimensionSpecialEffects {
 
@@ -75,12 +84,19 @@ public class TADimensionSpecialEffects extends DimensionSpecialEffects {
 
     public static class Mars extends TADimensionSpecialEffects
     {
+        @Nullable
+        private VertexBuffer skyBuffer;
+
+        @Nullable
+        private VertexBuffer starBuffer;
+
+        @Nullable
+        private VertexBuffer darkBuffer;
+
         public Mars()
         {
-            super(Float.NaN, true, SkyType.NONE, false, false);
+            super(Float.NaN, true, SkyType.NORMAL, false, false);
         }
-
-
 
         @Override
         public Vec3 getBrightnessDependentFogColor(Vec3 biomeFogColor, float daylight) {
@@ -89,12 +105,101 @@ public class TADimensionSpecialEffects extends DimensionSpecialEffects {
 
         @Override
         public boolean isFoggyAt(int x, int y) {
-            return true;
+            return false;
         }
 
         @Override
         public boolean renderSky(ClientLevel level, int ticks, float partialTick, PoseStack poseStack, Camera camera, Matrix4f projectionMatrix, boolean isFoggy, Runnable setupFog)
         {
+            if(ModList.get().isLoaded("stellarview"))
+            {
+                return ViewCenters.renderViewCenterSky(level, ticks, partialTick, poseStack, camera, projectionMatrix, isFoggy, setupFog);
+            }
+            Vec3 vec3 = level.getSkyColor(Minecraft.getInstance().gameRenderer.getMainCamera().getPosition(), partialTick);
+            float f = (float)vec3.x;
+            float f1 = (float)vec3.y;
+            float f2 = (float)vec3.z;
+            FogRenderer.levelFogColor();
+            BufferBuilder bufferbuilder = Tesselator.getInstance().getBuilder();
+            RenderSystem.depthMask(false);
+            RenderSystem.setShaderColor(f, f1, f2, 1.0F);
+            ShaderInstance shaderinstance = RenderSystem.getShader();
+            this.skyBuffer.bind();
+            this.skyBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, shaderinstance);
+            VertexBuffer.unbind();
+            RenderSystem.enableBlend();
+            float[] afloat = level.effects().getSunriseColor(level.getTimeOfDay(partialTick), partialTick);
+            if (afloat != null) {
+                RenderSystem.setShader(GameRenderer::getPositionColorShader);
+                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                poseStack.pushPose();
+                poseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
+                float f3 = Mth.sin(level.getSunAngle(partialTick)) < 0.0F ? 180.0F : 0.0F;
+                poseStack.mulPose(Axis.ZP.rotationDegrees(f3));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(90.0F));
+                float f4 = afloat[0];
+                float f5 = afloat[1];
+                float f6 = afloat[2];
+                Matrix4f matrix4f = poseStack.last().pose();
+                bufferbuilder.begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION_COLOR);
+                bufferbuilder.vertex(matrix4f, 0.0F, 100.0F, 0.0F).color(f4, f5, f6, afloat[3]).endVertex();
+                int i = 16;
+
+                for(int j = 0; j <= 16; ++j) {
+                    float f7 = (float)j * ((float)Math.PI * 2F) / 16.0F;
+                    float f8 = Mth.sin(f7);
+                    float f9 = Mth.cos(f7);
+                    bufferbuilder.vertex(matrix4f, f8 * 120.0F, f9 * 120.0F, -f9 * 40.0F * afloat[3]).color(afloat[0], afloat[1], afloat[2], 0.0F).endVertex();
+                }
+
+                BufferUploader.drawWithShader(bufferbuilder.end());
+                poseStack.popPose();
+            }
+
+            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+            poseStack.pushPose();
+            float f11 = 1.0F - level.getRainLevel(partialTick);
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, f11);
+            poseStack.mulPose(Axis.YP.rotationDegrees(-90.0F));
+            poseStack.mulPose(Axis.XP.rotationDegrees(level.getTimeOfDay(partialTick) * 360.0F));
+            Matrix4f matrix4f1 = poseStack.last().pose();
+            float f12 = 30.0F;
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderTexture(0, new ResourceLocation("textures/environment/sun.png"));
+            bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+            bufferbuilder.vertex(matrix4f1, -f12, 100.0F, -f12).uv(0.0F, 0.0F).endVertex();
+            bufferbuilder.vertex(matrix4f1, f12, 100.0F, -f12).uv(1.0F, 0.0F).endVertex();
+            bufferbuilder.vertex(matrix4f1, f12, 100.0F, f12).uv(1.0F, 1.0F).endVertex();
+            bufferbuilder.vertex(matrix4f1, -f12, 100.0F, f12).uv(0.0F, 1.0F).endVertex();
+            BufferUploader.drawWithShader(bufferbuilder.end());
+
+            float f10 = level.getStarBrightness(partialTick) * f11;
+            if (f10 > 0.0F) {
+                RenderSystem.setShaderColor(f10, f10, f10, f10);
+                FogRenderer.setupNoFog();
+                this.starBuffer.bind();
+                this.starBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, GameRenderer.getPositionShader());
+                VertexBuffer.unbind();
+                setupFog.run();
+            }
+
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+            RenderSystem.disableBlend();
+            RenderSystem.defaultBlendFunc();
+            poseStack.popPose();
+            RenderSystem.setShaderColor(0.0F, 0.0F, 0.0F, 1.0F);
+            double d0 = Minecraft.getInstance().player.getEyePosition(partialTick).y - level.getLevelData().getHorizonHeight(level);
+            if (d0 < 0.0D) {
+                poseStack.pushPose();
+                poseStack.translate(0.0F, 12.0F, 0.0F);
+                this.darkBuffer.bind();
+                this.darkBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, shaderinstance);
+                VertexBuffer.unbind();
+                poseStack.popPose();
+            }
+
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+            RenderSystem.depthMask(true);
             return true;
         }
     }
--- src/main/java/net/tadditions/mod/init/PhenomenaInit.java
+++ src/main/java/net/tadditions/mod/init/PhenomenaInit.java
@@ -1,6 +1,5 @@
 package net.tadditions.mod.init;
 
-import net.minecraft.world.level.ChunkPos;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -8,7 +7,6 @@ import net.tadditions.mod.TemporalAdditionsMod;
 import net.tadditions.mod.phenomena.EntropicDriftPhenomena;
 import net.tadditions.mod.phenomena.TimeStormPhenomena;
 import net.tardis.mod.helpers.Helper;
-import net.tardis.mod.misc.tardis.vortex.BaseFlightEventPhenomena;
 import net.tardis.mod.misc.tardis.vortex.VortexPhenomenaType;
 
 public class PhenomenaInit
